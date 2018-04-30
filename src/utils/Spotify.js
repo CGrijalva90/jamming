235b45ba013187c4d76c8a46b7d27b2837b2ec01
Carlos Grijalva <grijalva.carlos@gmail.com>
2018-04-30 12:37:21 -0400

    Complete saving playlist for user functionality

--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -24,7 +24,7 @@ const Spotify = {
   search(searchTerm) {
     const accessToken = Spotify.getAccessToken();
     const headers = { headers: { Authorization: `Bearer ${accessToken}` } };
-    fetch(
+    return fetch(
       `https://api.spotify.com/v1/search?type=track&q=${searchTerm}`,
       headers
     )
@@ -35,18 +35,49 @@ const Spotify = {
         if (!jsonResponse.tracks) {
           return [];
         }
-        return jsonResponse.tracks.items.map(track => {
-          return {
-            id: track.id,
-            name: track.name,
-            artist: track.artists[0].name,
-            album: track.album.name,
-            uri: track.uri
-          };
-        });
+        return jsonResponse.tracks.items.map(track => ({
+          id: track.id,
+          name: track.name,
+          artist: track.artists[0].name,
+          album: track.album.name,
+          uri: track.uri
+        }));
       })
       .catch(err => {
         console.log(err);
+      });
+  },
+
+  savePlaylist(name, trackUris) {
+    if (!name || !trackUris.length) {
+      return;
+    }
+
+    const accessToken = Spotify.getAccessToken();
+    const headers = { Authorization: `Bearer ${accessToken}` };
+    let userId;
+
+    return fetch('https://api.spotify.com/v1/me', { headers: headers })
+      .then(response => response.json())
+      .then(jsonResponse => {
+        userId = jsonResponse.id;
+        return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+          headers: headers,
+          method: 'POST',
+          body: JSON.stringify({ name: name })
+        })
+          .then(response => response.json())
+          .then(jsonResponse => {
+            const playlistId = jsonResponse.id;
+            return fetch(
+              `https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`,
+              {
+                headers: headers,
+                method: 'POST',
+                body: JSON.stringify({ uris: trackUris })
+              }
+            );
+          });
       });
   }
 };
